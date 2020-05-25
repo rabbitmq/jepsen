35afdfc69f49648257ebabaa1790797722d04def
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2020-05-25 14:50:03 +0200

    Bump Jepsen to 0.1.19
    And downgrade Clojure to 1.10.0 as Jepsen 0.1.19 is failing with Clojure
    1.10.1.

--- rabbitmq/project.clj
+++ rabbitmq/project.clj
@@ -8,6 +8,6 @@
   :java-source-paths ["src/main/java"]
   :javac-options     ["-target" "1.8" "-source" "1.8"]
   :jvm-opts ["-Dcom.sun.management.jmxremote"]
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [jepsen "0.1.18"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [jepsen "0.1.19"]
                  [com.rabbitmq/amqp-client "5.9.0" ]])
