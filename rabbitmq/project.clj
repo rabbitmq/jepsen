251f9a2d76870a268a40481cbde454a57ffcd20f
Arnaud Cogoluègnes <acogoluegnes@tuta.io>
2025-01-22 08:56:34 +0100

    Bump Java client to 5.24.0

--- rabbitmq/project.clj
+++ rabbitmq/project.clj
@@ -9,7 +9,7 @@
   :jvm-opts ["-Dcom.sun.management.jmxremote"]
   :dependencies [[org.clojure/clojure "1.12.0"]
                  [jepsen "0.3.7"]
-                 [com.rabbitmq/amqp-client "5.22.0"
+                 [com.rabbitmq/amqp-client "5.24.0"
                   :exclusions [org.slf4j/slf4j-api]]
                  ]
   :exclusions [org.slf4j/log4j-over-slf4j
