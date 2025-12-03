7d841f4c4b4145979244c0e907ebae5f92a4f9a0
Arnaud Cogoluègnes <514737+acogoluegnes@users.noreply.github.com>
2025-12-03 11:01:02 +0000

    Bump Jepsen to 0.3.10

--- rabbitmq/project.clj
+++ rabbitmq/project.clj
@@ -7,9 +7,9 @@
   :source-paths ["src/main/clojure"]
   :java-source-paths ["src/main/java"]
   :jvm-opts ["-Dcom.sun.management.jmxremote"]
-  :dependencies [[org.clojure/clojure "1.12.0"]
-                 [jepsen "0.3.9"]
-                 [com.rabbitmq/amqp-client "5.25.0"
+  :dependencies [[org.clojure/clojure "1.12.3"]
+                 [jepsen "0.3.10"]
+                 [com.rabbitmq/amqp-client "5.26.0"
                   :exclusions [org.slf4j/slf4j-api]]
                  ]
   :exclusions [org.slf4j/log4j-over-slf4j
